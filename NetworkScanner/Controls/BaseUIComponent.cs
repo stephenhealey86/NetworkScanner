52e158b9a040f7358eab912444334cde8b9b581b
Stephen Healey <s_healey@hotmail.co.uk>
2020-03-14 20:03:13 +0000

    Added settings dialog

---
+++ NetworkScanner/Controls/BaseUIComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NetworkScanner
+{
+    public class BaseUIComponent<VM> : UserControl where VM : BaseViewModel, new()
+    {
+        private VM mViewModel;
+
+        public VM ViewModel
+        {
+            get { return mViewModel; }
+
+            set
+            {
+                // If nothing has changed
+                if (mViewModel == value)
+                    return;
+                // Update value
+                mViewModel = value;
+                // Set the data context
+                this.DataContext = mViewModel;
+            }
+        }
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BaseUIComponent()
+        {
+            NewViewModel();
+        }
+        #endregion
+
+        public void NewViewModel()
+        {
+            ViewModel = new VM();
+        }
+    }
+}
---
+++ NetworkScanner/Controls/myNumericEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NetworkScanner
+{
+    public class myNumericEntry : TextBox
+    {
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+
+            var key = e.Key;
+            if ((key >= Key.D0 && key <= Key.D9) || (key >= Key.NumPad0 && key <= Key.NumPad9))
+            {
+                return;
+            } else if (key == Key.Delete || key == Key.Back)
+            {
+                return;
+            }
+            else if (key == Key.Left || key == Key.Right)
+            {
+                return;
+            }
+            else if (key == Key.Tab || key == Key.Enter)
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+    }
+}
